d434a13fd76718600069aa233ad8c2f1690b711c
Artem <dunaev.aa@yandex.ru>
2018-12-29 11:03:39 +0500

    get started with webpack

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # webpack-artem
+
+1. npm i
+2. webpack
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import sumFunction from './sumFunction';
+
+const a = 10;
+const b = 20;
+
+console.log(`Sum of ${a} and ${b} is`, sumFunction(a, b));
